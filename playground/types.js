c11003be600384f6bb0dfd5f4326071a93314a9c
eneax <19818937+eneax@users.noreply.github.com>
2020-01-17 21:56:42 +0100

    Quotes in JS

--- playground/types.js
+++ playground/types.js
@@ -1,1 +1,6 @@
-console.log('works');
+/* eslint-disable */
+const name = 'Enea';
+const middle = "X";
+const last = `Xharja`;
+
+const sentence = 'it's so cool!' // Uncaught SyntaxError: Unexpected identifier
