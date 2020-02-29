2bc29c738be203310207ba5794fdea257ba4aa89
eneax <19818937+eneax@users.noreply.github.com>
2020-02-29 19:38:20 +0100

    Introduce typeOf

--- playground/numbers/numbers.js
+++ playground/numbers/numbers.js
@@ -2,3 +2,13 @@ const age = 27;
 const name = 'enea';
 
 console.log(`My name is ${name} and I'm ${age} years old`);
+
+/*
+* typeOf
+The keyword `typeOf` allows you to check what is the 'type of' the value you have to deal with.
+
+```
+typeOf age // "number"
+typeOf name // "string"
+```
+*/
