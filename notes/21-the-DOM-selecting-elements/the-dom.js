6273427ac9fcce058f5f78173b338a401d654a36
eneax <19818937+eneax@users.noreply.github.com>
2020-06-25 21:44:51 +0200

    add example

---
+++ notes/21-the-DOM-selecting-elements/the-dom.js
@@ -0,0 +1,19 @@
+const p = document.querySelector('p');
+console.log(p);
+
+const divs = document.querySelector('div');
+console.log(divs);
+
+const items = document.querySelector('.item');
+console.log(items);
+
+const itemDivs = document.querySelector('div.item');
+console.log(itemDivs);
+
+const imgInsideItemDivs = document.querySelector('div.item img');
+console.log(imgInsideItemDivs);
+
+const item2 = document.querySelector('.item2');
+const item2Img = item2.querySelector('img');
+console.log(item2);
+console.log(item2Img);
